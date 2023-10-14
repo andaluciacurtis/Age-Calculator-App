33cc98c570d0e91852a34cd7a100baf2fd3d9e2e
andaluciacurtis <andalucia.curtis@gmail.com>
2023-10-13 17:19:13 -0700

    Added code to reset error messages

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const monthInputContainer = document.querySelector(".month-container");
 const yearInputContainer = document.querySelector(".year-container");
 
 enterButton.addEventListener("click", ()=>{
-  reset();
+  resetAll();
   calculateAge();
 });
 
@@ -25,20 +25,43 @@ function calculateAge() {
   
   if (checkDate(dayInput, monthInput, yearInput)) {
     console.log("yeah that works");
+    resetErrors();
 
     yearResult.innerHTML = currentYear - yearInput;
   }
 }
 
-function reset() {
-  errorText = document.querySelectorAll(".error");
-  errorText.forEach(function(section) {
-    section.innerHTML = "";
+function resetAll() {
+  resetErrors();
+  dayResult.innerHTML = "--";
+  monthResult.innerHTML = "--";
+  yearResult.innerHTML = "--";
+}
+
+function resetErrors() {
+  containers = {dayInputContainer, monthInputContainer, yearInputContainer};
+  
+  containers.forEach(function(container) {
+    container.querySelector("h2").style.color = "hsl(0, 1%, 44%)";
+    container.querySelector("input").style.border = "1px solid hsl(0, 0%, 94%)";
+    container.querySelector(".error").innerHTML = "";
   })
 }
 
 function checkDate(day, month, year) {
   let isValid = true;
+
+  // checks if any fields are empty
+  if (day === "") {
+    returnError(dayInputContainer, "This field is required");
+    isValid = false;
+  } else if (month === "") {
+    returnError(monthInputContainer, "This field is required");
+    isValid = false;
+  } else if (year === "") {
+    returnError(yearInputContainer, "This field is required");
+    isValid = false;
+  }
 
   // checks if the year is a leap year
   // leap years are either multiples of 400, of multiples of 4 and not of 100
@@ -46,8 +69,7 @@ function checkDate(day, month, year) {
 
   // checks if month if valid
   if (month < 1 || month > 12) {
-    errorText = monthInputContainer.querySelector(".error");
-    errorText.innerHTML = "Must be a valid month.";
+    returnError(monthInputContainer, "Must be a valid month.");
     isValid = false;
   }
 
@@ -55,17 +77,24 @@ function checkDate(day, month, year) {
   if (day > 31 || 
     ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30) || 
     (month == 2 && ((isLeapYear && day > 29) || (!isLeapYear && day > 28)))) {
-    errorText = dayInputContainer.querySelector(".error");
-    errorText.innerHTML = "Must be a valid day.";
-    isValid = false;
+      returnError(dayInputContainer, "Must be a valid day.");
+      isValid = false;
   }
 
   // checking to see if the input date is greater than today's date
   if (year > currentYear || (year == currentYear && (month > currentMonth || (month == currentMonth && day > currentDay)))) {
-    errorText = yearInputContainer.querySelector(".error");
-    errorText.innerHTML = "Must be in the past."
-    isValid = false;
+    returnError(yearInputContainer, "Must be in the past.");
+      isValid = false;
   }
 
   return isValid;
+}
+
+function returnError(field, errorMessage) {
+  errorText = field.querySelector(".error");
+  errorText.innerHTML = errorMessage;
+
+  // changing the field's title and input box to red
+  field.querySelector("h2").style.color = "hsl(0, 100%, 67%)";
+  field.querySelector("input").style.border = "1px solid hsl(0, 100%, 67%)";
 }
